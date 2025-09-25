1e3a4e051ed2f60a3dba886309e44d48fab602ea
k.rogachev <kirya.rog@ya.ru>
2025-09-25 09:05:27 +0300

    feat: add Task and TaskStatus models

---
+++ internal/task/task.go
@@ -0,0 +1,43 @@
+package task
+
+import (
+	"encoding/json"
+	"time"
+)
+
+type TaskStatus uint
+
+const (
+	StatusPending TaskStatus = iota
+	StatusRunning
+	StatusCompleted
+	StatusFailed
+)
+
+func (s TaskStatus) String() string {
+	switch s {
+	case StatusPending:
+		return "pending"
+	case StatusRunning:
+		return "running"
+	case StatusCompleted:
+		return "completed"
+	case StatusFailed:
+		return "failed"
+	default:
+		return "unknown"
+	}
+}
+
+func (s TaskStatus) MarshalJSON() ([]byte, error) {
+	return json.Marshal(s.String())
+}
+
+type Task struct {
+	ID        string     `json:"id"`
+	URLs      []string   `json:"urls"`
+	Status    TaskStatus `json:"status"`
+	CreatedAt time.Time  `json:"created_at"`
+	UpdatedAt time.Time  `json:"updated_at"`
+	Error     string     `json:"error"`
+}
